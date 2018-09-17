b85ef56c103cbd00553c048202ee20ea668de77f
Sky Richardson <skyricho@gmail.com>
2018-09-17 16:13:04 +1000

    Remove if statement

--- receive.php
+++ receive.php
@@ -6,36 +6,28 @@ $loader = new Twig_Loader_Filesystem('views');
 $twig = new Twig_Environment($loader);
 $template = $twig->load('receive.html.twig');
 
-if (isset($_GET['cupboard'])) {
-	$request = $fm->newFindCommand('web');
-    $request->addFindCriterion('INVENTORY::requested', '*'; 
-    $result = $request->execute();
 
-    if (FileMaker::isError($result)) {
-//        $ggg = 'foo';
-//        echo  "<p>Error: " . $result->getMessage() . "</p>"; exit;
-        $msg = $result->getMessage();
-    } else {
-        $records = $result->getRecords();
+$request = $fm->newFindCommand('web');
+$request->addFindCriterion('INVENTORY::requested', '*'; 
+$result = $request->execute();
+$records = $result->getRecords();
 
-        $publications = array();
-        foreach($records as $record) {
-            $publications[] = array(
-                'title' => $record->getField('title'),
-                'recID' => $record->getField('recID'),
-                'requested' => $record->getField('INVENTORY::requested'),
-                'shipped' => $record->getField('INVENTORY::shipped'),
-                //'note' => $record->getField('note'),
-                'symbol' => $record->getField('_symbol'),
-            );
-        }
-    }
-} 
+$publications = array();
+foreach($records as $record) {
+    $publications[] = array(
+        'title' => $record->getField('title'),
+        'recID' => $record->getField('recID'),
+        'requested' => $record->getField('INVENTORY::requested'),
+        'shipped' => $record->getField('INVENTORY::shipped'),
+        //'note' => $record->getField('note'),
+        'symbol' => $record->getField('_symbol'),
+    );
+}
 
 echo $template->render(array(
         'publications' => $publications,
         //'cupboard' => $_GET['cupboard'],
-        'msg' => $msg,
+        //'msg' => $msg,
         )
     );
 ?>
