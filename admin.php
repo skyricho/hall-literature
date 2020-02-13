99fe918aa5332ca4daaaaa4cabeb17e23855864c
Sky Richardson <skyricho@gmail.com>
2020-02-13 12:44:10 +1100

    Add admin form

--- admin.php
+++ admin.php
@@ -2,24 +2,51 @@
 ini_set('display_errors', 1);
 include ("dbaccess.php"); 
 
- 
-# Check the POST id and update the shipped field.
-if ($_POST['submit'] == 'updateOnHand') {
+$saved = '<div class=\"alert alert-info alert-dismissible fade show\" role=\"alert\">
+      Saved.
+      <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+        <span aria-hidden=\"true\">&times;</span>
+      </button></div>';
+
+// Update onHandEnd
+if ($_POST['submit'] == 'onHandEnd') {
     $edit = $fm->newEditCommand('web', $_POST['id']);
     $edit->setField('Inventory::onHandEnd', $_POST['onHandEnd']);
     $edit->execute();
-} else {
-    echo 'Houston we have a problem'; 
+    echo stripslashes($saved);
 }
 
-# Render onHandEnd value
-$record = $fm->getRecordByID('web', $_POST['id']);
-$onHandEnd = $record->getField('Inventory::onHandEnd');
-echo '<div class=/"alert alert-info alert-dismissible fade show/" role=/"alert/">
-  Saved.
-  <button type=/"button/" class=/"close/" data-dismiss=/"alert/" aria-label=/"Close/">
-    <span aria-hidden=/"true/">&times;</span>
-  </button>';
+// Update proposed
+if ($_POST['submit'] == 'proposed') {
+    $edit = $fm->newEditCommand('web', $_POST['id']);
+    $edit->setField('Inventory::proposed', $_POST['proposed']);
+    $edit->execute();
+    echo stripslashes($saved);
+}
+
+// Update requested
+if ($_POST['submit'] == 'requested') {
+    $edit = $fm->newEditCommand('web', $_POST['id']);
+    $edit->setField('Inventory::requested', $_POST['requested']);
+    $edit->execute();
+    echo stripslashes($saved);
+}
+
+// Update received
+if ($_POST['submit'] == 'received') {
+    $edit = $fm->newEditCommand('web', $_POST['id']);
+    $edit->setField('Inventory::received', $_POST['received']);
+    $edit->execute();
+    echo stripslashes($saved);
+}
+
+// Update location
+if ($_POST['submit'] == 'location') {
+    $edit = $fm->newEditCommand('web', $_POST['id']);
+    $edit->setField('cupboardLocation', $_POST['cupboard']);
+    $edit->execute();
+    echo stripslashes($saved);
+}
 
 ?>
 
